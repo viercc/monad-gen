3d2ccb99d7d8fe69b76d63a1c5a1a708d88ca211
Koji Miyazato <viercc@gmail.com>
2023-12-02 15:27:16 +0900

    Output isomorphism classes
    output/*groups.txt

--- monad-gen/app/Main.hs
+++ monad-gen/app/Main.hs
@@ -8,8 +8,10 @@
 
 module Main (main) where
 
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
+
 import Data.Foldable
-import qualified Data.LazyVec as Vec
 import Data.PTraversable
 import Data.PTraversable.Extra
 import Data.Proxy
@@ -25,69 +27,118 @@ import Util
 ------------------------
 -- Tests
 
-forAll :: (Foldable t) => t a -> (a -> Bool) -> Bool
-forAll = flip all
+monoidGen ::
+  forall f.
+  ( forall a. (Show a) => Show (f a),
+    PTraversable f
+  ) =>
+  Proxy f ->
+  (String -> IO ()) ->
+  IO [ (String, MonoidData f) ]
+monoidGen _ println = do
+  let monoidNames = [ "M_" ++ show i | i <- [ 1 :: Int .. ] ]
+      monoids = zip monoidNames genMonoids
+  for_ monoids $ \(monoidName, monData) -> mapM_ println (prettyMonoidData monoidName monData)
+  pure monoids
 
 monadGen ::
   forall f.
-  ( forall a. (Ord a) => Ord (f a),
-    forall a. (Show a) => Show (f a),
+  ( forall a. (Show a) => Show (f a),
     PTraversable f
   ) =>
   Proxy f ->
   (String -> IO ()) ->
   IO ()
-monadGen _ println = do
-  let monoidNames = [ "M_" ++ show i | i <- [ 1 :: Int .. ] ]
-      monadNames = [ "Monad_" ++ show i | i <- [ 1 :: Int ..] ]
-      monoids = zip monoidNames genMonoids
+monadGen proxy println = do
+  monoids <- monoidGen proxy println
+  let monadNames = [ "Monad_" ++ show i | i <- [ 1 :: Int ..] ]
+      monads :: [ (String, MonadData f) ]
       monads = do
         (monoidName, monData) <- monoids
         monadData <- uniqueByIso (concat makePositionIsoFactors) $ genFromMonoid (makeMonoidDict monData)
         pure (monoidName, monadData)
-  for_ monoids $ \(monoidName, monData) -> mapM_ println (prettyMonoidData monoidName monData)
-  for_ (zip monadNames monads) docResult
+  for_ (zip monadNames monads) $ \(monadName, (monoidName, monadData)) -> do
+    let dict = makeMonadDict monadData
+    validateMonadDict dict
+    mapM_ println $ prettyMonadDict monadName monoidName dict
+
+monadGenGroup ::
+  forall f.
+  ( forall a. (Show a) => Show (f a),
+    PTraversable f
+  ) =>
+  Proxy f ->
+  (String -> IO ()) ->
+  IO ()
+monadGenGroup proxy println = do
+  monoids <- monoidGen proxy println
+  let monadNames = [ "Monad_" ++ show i | i <- [ 1 :: Int ..] ]
+      monads :: [ (String, [MonadData f]) ]
+      monads = do
+        (monoidName, monData) <- monoids
+        monadDataGroup <- groupByIso (concat makePositionIsoFactors) $ genFromMonoid (makeMonoidDict monData)
+        pure (monoidName, toList monadDataGroup)
+  for_ (zip monadNames monads) $ \(monadName, (monoidName, monadDataGroup)) -> do
+    let dicts = makeMonadDict <$> monadDataGroup
+    mapM_ validateMonadDict dicts
+    let
+      prettyGroup = ["IsomorphismClass {"]
+        ++ map ("  " <>) (concatMap (prettyMonadDict monadName monoidName) dicts)
+        ++ ["}"]
+    mapM_ println prettyGroup
+
+validateMonadDict :: forall f.
+     (PTraversable f, forall a. Show a => Show (f a))
+  => MonadDict f -> IO ()
+validateMonadDict MonadDict{ _monadPure = pure', _monadJoin = join' }
+   = if null allFails
+       then pure ()
+       else dieWithErrors allFails
   where
-    skolemCache :: Vec (f Int)
-    skolemCache = cache skolem
+    skolemCache :: [f Int]
+    skolemCache = toList skolem
 
-    skolem2Cache :: Vec (f (f Int))
-    skolem2Cache = cache skolem2
+    skolem3Cache :: [f (f (f Int))]
+    skolem3Cache = toList skolem3
+ 
+    leftUnitFails = 
+      [ "leftUnit " ++ show fx | fx <- skolemCache, not (checkLeftUnit pure' join' fx) ]
+    rightUnitFails = 
+      [ "rightUnit " ++ show fx | fx <- skolemCache, not (checkRightUnit pure' join' fx) ]
+    assocFails = 
+      [ "assoc " ++ show fffx | fffx <- skolem3Cache, not (checkAssoc join' fffx) ]
 
-    skolem3Cache :: Vec (f (f (f Int)))
-    skolem3Cache = cache skolem3
+    allFails = leftUnitFails ++ rightUnitFails ++ assocFails
 
-    validate :: MonadDict f -> IO ()
-    validate MonadDict{ _monadPure = pure', _monadJoin = join' } =
-      if and allLaws
-        then return ()
-        else fail $ "[leftUnit, rightUnit, assoc] = " ++ show allLaws
-      where
-        leftUnitLaw = forAll skolemCache $ checkLeftUnit pure' join'
-        rightUnitLaw = forAll skolemCache $ checkRightUnit pure' join'
-        assocLaw = forAll skolem3Cache $ checkAssoc join'
-        allLaws = [leftUnitLaw, rightUnitLaw, assocLaw]
+dieWithErrors :: [String] -> IO a
+dieWithErrors errs = mapM_ (hPutStrLn stderr) errs >> exitFailure
 
-    joinArgsCache :: Vec String
-    joinArgsCache = cache $ fmap pad strs
+prettyMonadDict :: forall f.
+     (PTraversable f, forall a. Show a => Show (f a))
+  => String -> String -> MonadDict f -> [String]
+prettyMonadDict = docResult
+  where
+    skolem2Cache :: [f (f Int)]
+    skolem2Cache = toList $ skolem2
+    
+    joinArgsCache :: [String]
+    joinArgsCache = pad <$> strs
       where
         showLen x = let s = show x in (length s, s)
-        strs = cache $ showLen <$> skolem2Cache
-        maxLen = maximum $ fmap fst strs
+        strs = showLen <$> skolem2Cache
+        maxLen = maximum $ (0 : fmap fst strs)
         pad (n, s) = "join $ " ++ s ++ replicate (maxLen - n) ' ' ++ " = "
-
-    docResult (monadName, (monoidName, monadData)) = do
-        validate dict
-        println $ monadName ++ " = Monad{"
-        println $ indent <> "baseMonoid = " ++ monoidName
-        println $ indent <> "pure 0 = " <> show (_monadPure dict (0 :: Int))
-        let docLine s ffx = indent <> s <> show (_monadJoin dict ffx)
-        mapM_ println $
-          Vec.zipWith docLine joinArgsCache skolem2Cache
-        println "}\n"
-      where
-        indent = "    "
-        dict = makeMonadDict monadData
+    
+    indent = "  "
+    
+    docResult monadName monoidName dict =
+        [ monadName <> " = Monad{" ] ++
+        map (indent <>) (
+          [ "baseMonoid = " ++ monoidName,
+            "pure 0 = " <> show (_monadPure dict (0 :: Int)) ] ++
+          zipWith (<>) joinArgsCache (show . _monadJoin dict <$> skolem2Cache)
+        ) ++
+        ["}"]
 
 main :: IO ()
 main = do
@@ -101,6 +152,8 @@ main = do
   writeFile' "output/I.txt" $ monadGen @I Proxy
   writeFile' "output/J.txt" $ monadGen @J Proxy
   writeFile' "output/T.txt" $ monadGen @T Proxy
+  writeFile' "output/Tgroups.txt" $ monadGenGroup @T Proxy
   writeFile' "output/U.txt" $ monadGen @U Proxy
+  writeFile' "output/Ugroups.txt" $ monadGenGroup @U Proxy
   writeFile' "output/V.txt" $ monadGen @V Proxy
-  --writeFile' "output/Y.txt" $ monadGen @Y Proxy
+  writeFile' "output/Y.txt" $ monadGen @Y Proxy
