25e18c66793e5e8e47bfdca428ae429814063c22
Koji Miyazato <viercc@gmail.com>
2023-12-20 18:25:03 +0900

    Update with Ord1 => PTraversable change

--- monad-gen/lib/Targets.hs
+++ monad-gen/lib/Targets.hs
@@ -6,52 +6,53 @@
 module Targets where
 
 import GHC.Generics
+import Data.Functor.Classes (Eq1, Ord1)
 import Data.PTraversable
 
 data F a = F | Fa a a
   deriving stock (Show, Eq, Ord, Functor, Foldable, Traversable, Generic1)
-deriving via (Generically1 F) instance PTraversable F
+  deriving (Eq1, Ord1, PTraversable) via (Generically1 F)
 
 data G a = Ga a | Gb a a
   deriving stock (Show, Eq, Ord, Functor, Foldable, Traversable, Generic1)
-deriving via (Generically1 G) instance PTraversable G
+  deriving (Eq1, Ord1, PTraversable) via (Generically1 G)
 
 data H a = H | Ha a | Hb a a
   deriving stock (Show, Eq, Ord, Functor, Foldable, Traversable, Generic1)
-deriving via (Generically1 H) instance PTraversable H
+  deriving (Eq1, Ord1, PTraversable) via (Generically1 H)
 
 data I a = Ia a | Ib a a a
   deriving stock (Show, Eq, Ord, Functor, Foldable, Traversable, Generic1)
-deriving via (Generically1 I) instance PTraversable I
+  deriving (Eq1, Ord1, PTraversable) via (Generically1 I)
 
 data J a = Ja a | Jb a | Jc a a
   deriving stock (Show, Eq, Ord, Functor, Foldable, Traversable, Generic1)
-deriving via (Generically1 J) instance PTraversable J
+  deriving (Eq1, Ord1, PTraversable) via (Generically1 J)
 
 data K a = K | K' | Ka a | Kb a
   deriving stock (Show, Eq, Ord, Functor, Foldable, Traversable, Generic1)
-deriving via (Generically1 K) instance PTraversable K
+  deriving (Eq1, Ord1, PTraversable) via (Generically1 K)
 
 data L a = La a a a | Lb a a a
   deriving stock (Show, Eq, Ord, Functor, Foldable, Traversable, Generic1)
-deriving via (Generically1 L) instance PTraversable L
+  deriving (Eq1, Ord1, PTraversable) via (Generically1 L)
 
 data T a = Ta a a | Tb a a
   deriving stock (Show, Eq, Ord, Functor, Foldable, Traversable, Generic1)
-deriving via (Generically1 T) instance PTraversable T
+  deriving (Eq1, Ord1, PTraversable) via (Generically1 T)
 
 data U a = Ua a a | Ub a a | Uc a a
   deriving stock (Show, Eq, Ord, Functor, Foldable, Traversable, Generic1)
-deriving via (Generically1 U) instance PTraversable U
+  deriving (Eq1, Ord1, PTraversable) via (Generically1 U)
 
 data W a = Wa a a | Wb a a | Wc a a | Wd a a
   deriving stock (Show, Eq, Ord, Functor, Foldable, Traversable, Generic1)
-deriving via (Generically1 W) instance PTraversable W
+  deriving (Eq1, Ord1, PTraversable) via (Generically1 W)
 
 data V a = V | Va a | Vb a | Vc a a
   deriving stock (Show, Eq, Ord, Functor, Foldable, Traversable, Generic1)
-deriving via (Generically1 V) instance PTraversable V
+  deriving (Eq1, Ord1, PTraversable) via (Generically1 V)
 
 data Y a = Y | Yaa a | Yab a | Yba a | Ybb a | Yc a a
   deriving stock (Show, Eq, Ord, Functor, Foldable, Traversable, Generic1)
-deriving via (Generically1 Y) instance PTraversable Y
+  deriving (Eq1, Ord1, PTraversable) via (Generically1 Y)
