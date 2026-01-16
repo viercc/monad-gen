f8d2d8be8e2b654ab886d1fd25b0b30e83bfcc28
Koji Miyazato <viercc@gmail.com>
2026-01-16 13:54:23 +0900

    Use -O2 in measure.sh

--- monad-gen/measure.sh
+++ monad-gen/measure.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-targets="F G H I J K T U V"
+targets="F G H I J K T U V I'"
 log_file="./measured_times"
 time_format="\"%C\",%U,%e,%Mk"
 
-cabal build monad-gen || exit 1
+cabal build monad-gen -O2 || exit 1
 for target in $targets; do
   echo $target
-  cabal exec -- /usr/bin/time -a -o "$log_file" -f "$time_format" monad-gen $target
+  cabal exec -O2 -- /usr/bin/time -a -o "$log_file" -f "$time_format" monad-gen $target
 done
