918c934953bab1f477725e57421f2b50bcc8f718
Koji Miyazato <viercc@gmail.com>
2026-01-29 18:39:49 +0900

    Cut cartesian-profunctors for the new repo
    https://github.com/viercc/cartesian-profunctors

--- cartesian-profunctors/src/Data/Profunctor/Extra.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE GADTSyntax #-}
-module Data.Profunctor.Extra where
-
-import Data.Functor.Contravariant
-import Data.Bifunctor.Clown
-
-type EquivalenceP = Clown Equivalence
-type ComparisonP = Clown Comparison
